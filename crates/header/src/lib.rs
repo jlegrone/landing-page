efc7df2266d51583e9c3ec77fac6c095d585f99e
Jacob LeGrone <git@jacob.work>
2019-04-06 11:47:31 -0400

    WIP

--- crates/header/src/lib.rs
+++ crates/header/src/lib.rs
@@ -1,6 +1,11 @@
 extern crate js_sys;
 extern crate wasm_bindgen;
 extern crate web_sys;
+extern crate wee_alloc;
 
 mod canvas;
 mod util;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
